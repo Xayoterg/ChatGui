bd31562714217ef47b933431f5193ddc94bbce82
Hayoterg <khat.ruts@bk.ru>
2022-09-19 00:40:21 +0300

    исправлен бак

--- src/main/java/com/example/chatgui/HelloController.java
+++ src/main/java/com/example/chatgui/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.chatgui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,9 +12,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class HelloController {
@@ -26,19 +25,65 @@ public class HelloController {
     Button connectBtn;
     @FXML
     TextField textField;
-
-    @FXML
-    TextArea textAreaContact;
     @FXML
     VBox usersListVBox;
+    boolean isAuth = false;
+    String login = null;
+    String pass = null;
+    int to_id = 0;
+
+    protected void aut() throws IOException {
+        String token = "";
+        try {
+            FileReader reader = new FileReader("C//java/token.txt");
+            int i;
+            while ((i = reader.read()) != -1)
+                token += (char) i;
+        } catch (IOException e) {
+            System.out.println("token not found");
+
+        }
+        if (token.equals("")) {
+            textArea.appendText("Введите логин\n");
+
+        } else {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("login", "");
+            jsonObject.put("pass", "");
+            jsonObject.put("token", "");
+            out.writeUTF(jsonObject.toJSONString());
+        }
+    }
+
     @FXML
 
     protected void handlerSend() throws IOException {
         String text = textField.getText();
+        if (text.equals("")) return;
+
         textField.clear();
         textField.requestFocus();
         textArea.appendText(text + "\n");
-        out.writeUTF(text);
+        if (isAuth) {
+            JSONObject request = new JSONObject();
+            request.put("msg", text);
+            request.put("to_id",to_id);
+            out.writeUTF(request.toJSONString());
+        } else {
+            if (login == null) {
+                login = text;
+                textArea.appendText("Введите пароль:\n");
+            } else if (pass == null) {
+                pass = text;
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("login", login);
+                jsonObject.put("pass", pass);
+                jsonObject.put("token", "");
+                out.writeUTF(jsonObject.toJSONString());
+                login = null;
+                pass = null;
+            }
+        }
     }
 
     @FXML
@@ -54,30 +99,77 @@ public class HelloController {
                 public void run() {
                     while (true) {
                         try {
+                            if (!isAuth) aut();
+
                             String response = is.readUTF();
                             JSONParser jsonParser = new JSONParser();
                             JSONObject jsonResponse = (JSONObject) jsonParser.parse(response);
+                            System.out.println(jsonResponse.get("authResult"));
                             if (jsonResponse.get("users") != null) {
-                                textAreaContact.clear();
-
+                                usersListVBox.getChildren().removeAll();
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        usersListVBox.getChildren().clear();
+                                    }
+                                });
                                 JSONArray jsonArray = (JSONArray) jsonParser.parse(jsonResponse.get("users").toString());
                                 for (int i = 0; i < jsonArray.size(); i++) {
                                     JSONObject jsonUserInfo = (JSONObject) jsonParser.parse(jsonArray.get(i).toString());
                                     String name = jsonUserInfo.get("name").toString();
-                                    textAreaContact.appendText(name+"\n");
+                                    int id = Integer.parseInt(jsonUserInfo.get("id").toString());
+                                    Button userBtn = new Button();  //создаем кнопку
+                                    userBtn.setText(name);
+                                    userBtn.setOnAction(e -> {
+                                        textArea.appendText("Нажата кнопка\n");
+                                        JSONObject jsonObject = new JSONObject();
+                                        jsonObject.put("getHistoryMessage",id );
+                                        to_id = id;
+                                        textArea.clear();
+                                        try {
+                                            out.writeUTF(jsonObject.toJSONString());
+                                        } catch (IOException ex) {
+                                            ex.printStackTrace();
+                                        }
+                                    });
+                                    Platform.runLater(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            usersListVBox.getChildren().add(userBtn);
+                                        }
+                                    });
 
                                 }
-
-                            }else if (jsonResponse.get("msg")!=null){
-                                textArea.appendText(jsonResponse.get("msg").toString() + "\n");
-
+                                // проверяем не прислал ли нам сервер ключи
+                            } else if (jsonResponse.get("msg") != null) { //если он есть он не равен нул
+                                textArea.appendText(((JSONObject)jsonParser.parse(jsonResponse.get("msg").toString())).get("msg").toString()+ "\n");//отображаем на экране
+                            } else if (jsonResponse.get("authResult") != null) {//если он есть он не равен нул
+                                isAuth = jsonResponse.get("authResult").toString().equals("success");
+                                String token = jsonResponse.get("token").toString();
+                                FileOutputStream fos = new FileOutputStream("C//java/token.txt");
+                                byte[] buffer = token.getBytes();
+                                fos.write(buffer);
+                                fos.close();
+                            } else if (jsonResponse.get("messages") != null) {//если он есть он не равен нул
+                                JSONArray messages = (JSONArray) jsonParser.parse(jsonResponse.get("messages").toString());//если он есть парсим в массив
+                                for (int i = 0; i < messages.size(); i++) {        //считываем каждую по строчке
+                                    JSONObject message = (JSONObject) jsonParser.parse(messages.get(i).toString()); // распарсит каждый элемент
+                                    String name = message.get("name").toString(); //достаём имя,id,сообщение
+                                    String msg = message.get("msg").toString();
+                                    textArea.appendText(name + ":" + msg + "\n");//отображаем на экране
+                                }
+                            } else if (jsonResponse.get("privateMessages") != null) {
+                                JSONArray messages = (JSONArray) jsonParser.parse(jsonResponse.get("privateMessages").toString());
+                                for (int i = 0; i < messages.size(); i++) { //разбираем обьекты
+                                    JSONObject singleJsonMessage=(JSONObject) jsonParser.parse(messages.get(i).toString());
+                                    String msg=singleJsonMessage.get("msg").toString(); //сохраняем в переменную
+                                    textArea.appendText(msg+"\n");
+                                }
                             }
-
                         } catch (IOException | ParseException e) {
                             e.printStackTrace();
                         }
                     }
-
                 }
             });
             thread.start();
@@ -85,7 +177,5 @@ public class HelloController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }
